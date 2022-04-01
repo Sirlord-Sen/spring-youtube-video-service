ba5aad67ec1f508202c1fb725b736be951de09d3
Sir-Lord <60177652+Sirlord-Sen@users.noreply.github.com>
2022-04-01 14:23:45 +0000

    Initial commit

---
+++ src/test/java/com/example/youtubevideoservice/YoutubevideoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youtubevideoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubevideoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
